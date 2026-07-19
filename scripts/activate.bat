4b57ab861565a46046db76645c5625dd2221498f
Kimberly McGuire <kimberleymcguire@gmail.com>
2026-07-19 12:24:19 +0200

    add environment varibles to find plugin

--- scripts/activate.bat
+++ scripts/activate.bat
@@ -6,4 +6,13 @@ if exist "%PIXI_PROJECT_ROOT%\install\setup.bat" (
     call "%PIXI_PROJECT_ROOT%\install\setup.bat"
 )
 
+rem Work around FastDDS shared-memory lock/mutex failures on Windows.
+set "FASTDDS_BUILTIN_TRANSPORTS=UDPv4"
+
+rem Gazebo resource lookup paths (worlds/models/assets).
+set "GZ_SIM_RESOURCE_PATH=%CONDA_PREFIX%\Library\share\turtlebot4_gz_bringup\worlds;%CONDA_PREFIX%\Library\share\irobot_create_gz_bringup\worlds;%CONDA_PREFIX%\Library\share;%GZ_SIM_RESOURCE_PATH%"
+
+rem Gazebo GUI plugin lookup paths (TurtleBot4/Create3 UI plugins).
+set "GZ_GUI_PLUGIN_PATH=%CONDA_PREFIX%\Library\share\turtlebot4_gz_gui_plugins\lib;%CONDA_PREFIX%\Library\share\irobot_create_gz_plugins\lib;%GZ_GUI_PLUGIN_PATH%"
+
 set "GZ_SIM_SYSTEM_PLUGIN_PATH=%CONDA_PREFIX%\Library\lib;%GZ_SIM_SYSTEM_PLUGIN_PATH%"
